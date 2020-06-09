3980f751128f3bba8de23807a74488dc5fcab3de
Christopher Saunders <topher6345@gmail.com>
2020-06-08 22:33:42 -0700

    1591680822

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // prettier-ignore
+      board: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ]
+    };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        {[
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+        ].map((row) => {
+          return row.map((index) => {
+            const message = this.state.board[index];
+
+            if (message === "") {
+              return (
+                <td
+                  style={{
+                    backgroundColor: "red",
+                    maxWidth: "20px",
+                  }}
+                  onClick={() => {
+                    this.setState((state) => {
+                      state.board[index] = "X";
+                      return { board: state.board };
+                    });
+                  }}
+                  key={index}
+                >
+                  {index}
+                </td>
+              );
+            } else if (this.state.board[index] === "X") {
+              return (
+                <td
+                  style={{
+                    backgroundColor: "red",
+                    maxWidth: "20px",
+                  }}
+                  onClick={() => {
+                    this.setState((state) => {
+                      state.board[index] = "";
+                      return { board: state.board };
+                    });
+                  }}
+                  key={index + "X"}
+                >
+                  {"X"}
+                </td>
+              );
+            }
+          });
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
