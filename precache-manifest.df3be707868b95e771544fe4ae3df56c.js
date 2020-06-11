aa5d8adfeab11f9f4733327ed682a212375ae1d0
Christopher Saunders <topher6345@gmail.com>
2020-06-10 23:14:32 -0700

    Updates

---
+++ precache-manifest.df3be707868b95e771544fe4ae3df56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa9beec2ce72896621b0dbfc8214720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b14d736498c188fd69",
+    "url": "/static/css/main.c0fcea9b.chunk.css"
+  },
+  {
+    "revision": "730b7c3b284b79f7217c",
+    "url": "/static/js/2.b4f5193d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4f5193d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b14d736498c188fd69",
+    "url": "/static/js/main.4e0b08b8.chunk.js"
+  },
+  {
+    "revision": "46e1ce656ac57e3635d1",
+    "url": "/static/js/runtime-main.876764e1.js"
+  }
+]);
