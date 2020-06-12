1145d9afc7fe989ddc6f93ef3352aa0072c30d94
Christopher Saunders <topher6345@gmail.com>
2020-06-11 23:48:59 -0700

    1591944539

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const X = "❌";
+const O = "⭕️";
+const Empty = "";
+
 const indexsOf = (array, target) =>
   array.reduce((m, e, i) => (e === target ? m.concat(i) : m), []);
 
@@ -8,30 +12,90 @@ const count = (array, target) =>
   array.reduce((m, e) => (e === target ? m + 1 : m), 0);
 
 const detectWinner = (array) => {
-  if (array[0] === array[1] && array[1] === array[2] && array[2] !== "")
-    return array[0];
-  if (array[3] === array[4] && array[4] === array[5] && array[5] !== "")
-    return array[3];
-  if (array[6] === array[7] && array[7] === array[8] && array[8] !== "")
-    return array[6];
-  if (array[0] === array[3] && array[3] === array[6] && array[6] !== "")
-    return array[0];
-  if (array[1] === array[4] && array[4] === array[7] && array[7] !== "")
-    return array[1];
-  if (array[2] === array[5] && array[5] === array[8] && array[8] !== "")
-    return array[2];
-  if (array[0] === array[4] && array[4] === array[8] && array[8] !== "")
-    return array[0];
-  if (array[6] === array[4] && array[4] === array[2] && array[2] !== "")
-    return array[6];
+  const findWinner = (a, b, c) =>
+    array[a] === array[b] && array[b] === array[c] && array[c] !== Empty;
 
+  if (findWinner(0, 1, 2)) return array[0];
+  if (findWinner(3, 4, 5)) return array[3];
+  if (findWinner(6, 7, 8)) return array[6];
+  if (findWinner(0, 3, 6)) return array[0];
+  if (findWinner(1, 4, 7)) return array[1];
+  if (findWinner(2, 5, 8)) return array[2];
+  if (findWinner(0, 4, 8)) return array[0];
+  if (findWinner(6, 4, 2)) return array[6];
   if (count(array, "") === 0) return "Draw";
 
   return false;
 };
 
+// [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+// ]
+
+const blockStrategy = (board) => {
+  if (count(board, X) === 1 && board[4] === Empty) return 4;
+
+  const detect = (player, a, b, empty) =>
+    board[a] === player && board[b] === board[a] && board[empty] === Empty;
+
+  if (detect(O, 3, 6, 0)) return 0;
+  if (detect(O, 4, 8, 0)) return 0;
+  if (detect(O, 1, 2, 0)) return 0;
+  if (detect(O, 0, 2, 1)) return 1;
+  if (detect(O, 4, 7, 1)) return 1;
+  if (detect(O, 0, 1, 2)) return 2;
+  if (detect(O, 6, 4, 2)) return 2;
+  if (detect(O, 5, 8, 2)) return 2;
+  if (detect(O, 0, 6, 3)) return 3;
+  if (detect(O, 4, 5, 3)) return 3;
+  if (detect(O, 0, 8, 4)) return 4;
+  if (detect(O, 1, 7, 4)) return 4;
+  if (detect(O, 6, 2, 4)) return 4;
+  if (detect(O, 3, 5, 4)) return 4;
+  if (detect(O, 2, 8, 5)) return 5;
+  if (detect(O, 3, 4, 5)) return 5;
+  if (detect(O, 0, 3, 6)) return 6;
+  if (detect(O, 4, 2, 6)) return 6;
+  if (detect(O, 7, 8, 6)) return 6;
+  if (detect(O, 1, 4, 7)) return 7;
+  if (detect(O, 6, 8, 7)) return 7;
+  if (detect(O, 0, 4, 8)) return 8;
+  if (detect(O, 2, 5, 8)) return 8;
+  if (detect(O, 6, 7, 8)) return 8;
+
+  if (detect(X, 3, 6, 0)) return 0;
+  if (detect(X, 4, 8, 0)) return 0;
+  if (detect(X, 1, 2, 0)) return 0;
+  if (detect(X, 0, 2, 1)) return 1;
+  if (detect(X, 4, 7, 1)) return 1;
+  if (detect(X, 0, 1, 2)) return 2;
+  if (detect(X, 6, 4, 2)) return 2;
+  if (detect(X, 5, 8, 2)) return 2;
+  if (detect(X, 0, 6, 3)) return 3;
+  if (detect(X, 4, 5, 3)) return 3;
+  if (detect(X, 0, 8, 4)) return 4;
+  if (detect(X, 1, 7, 4)) return 4;
+  if (detect(X, 6, 2, 4)) return 4;
+  if (detect(X, 3, 5, 4)) return 4;
+  if (detect(X, 2, 8, 5)) return 5;
+  if (detect(X, 3, 4, 5)) return 5;
+  if (detect(X, 0, 3, 6)) return 6;
+  if (detect(X, 4, 2, 6)) return 6;
+  if (detect(X, 7, 8, 6)) return 6;
+  if (detect(X, 1, 4, 7)) return 7;
+  if (detect(X, 6, 8, 7)) return 7;
+  if (detect(X, 0, 4, 8)) return 8;
+  if (detect(X, 2, 5, 8)) return 8;
+  if (detect(X, 6, 7, 8)) return 8;
+
+  return [];
+};
+
+// String -> Array -> Number
 const playStrategy = (strategy, board) => {
-  const candidates = indexsOf(board, "");
+  const candidates = indexsOf(board, Empty);
   const length = candidates.length;
 
   if (strategy === "random") {
@@ -40,17 +104,33 @@ const playStrategy = (strategy, board) => {
     return candidates[0];
   } else if (strategy === "last") {
     return candidates[length - 1];
+  } else if (strategy === "block") {
+    const index = blockStrategy(board);
+    const candidate = candidates[Math.floor(Math.random() * length)];
+    if (JSON.stringify(index) === "[]") {
+      return candidate;
+    } else {
+      return index;
+    }
   }
 };
 
-const initalBoard = ["", "", "", "", "", "", "", "", ""];
+const initalBoard = [
+  Empty,
+  Empty,
+  Empty,
+  Empty,
+  Empty,
+  Empty,
+  Empty,
+  Empty,
+  Empty,
+];
 const initalState = {
   board: Array.from(initalBoard),
   over: false,
-  strategy: "random",
+  strategy: "block",
 };
-const X = "❌";
-const O = "⭕️";
 
 const App = () => {
   const [state, setState] = useState(initalState);
@@ -61,7 +141,8 @@ const App = () => {
       const board = Array.from(oldState.board);
       board[index] = X;
       const winner = detectWinner(board);
-      if (!winner) board[playStrategy(state.strategy, board)] = O;
+      const strategy = playStrategy(state.strategy, board);
+      if (!winner) board[strategy] = O;
       return { ...oldState, board, over: winner };
     });
   };
@@ -78,7 +159,10 @@ const App = () => {
     if (message === O) return <td key={index}>{O}</td>;
   };
 
-  const updateStrategy = (event) => setState({ strategy: event.target.value });
+  const updateStrategy = ({ target }) => {
+    const { value: strategy } = target;
+    setState((oldState) => ({ ...oldState, strategy }));
+  };
 
   const resetGame = () =>
     setState((oldState) => ({ ...initalState, strategy: oldState.strategy }));
@@ -110,7 +194,8 @@ const App = () => {
           <label htmlFor="strategy">Strategy</label>
         </p>
         <p>
-          <select name="strategy" onChange={updateStrategy}>
+          <select name="strategy" onChange={(e) => updateStrategy(e)}>
+            <option value="block">Block</option>
             <option value="random">RANDOM</option>
             <option value="first">FIRST</option>
             <option value="last">LAST</option>
@@ -124,8 +209,8 @@ const App = () => {
 export default App;
 
 const DrawModal = (props) => (
-  <div className="lose-modal">
-    <h2>😭You Lost!😭</h2>
+  <div className="draw-modal">
+    <h2>🤷‍♀️DRAW🤷‍♀️</h2>
     <button {...props}>Play Again</button>
   </div>
 );
